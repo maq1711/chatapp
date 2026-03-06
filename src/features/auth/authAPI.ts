02334323a3460a71c35fd22b575daf301929a845
Muhammad Ali Qureshi <ali.qureshi@logicielservice.com>
2026-03-06 13:08:26 +0500

    some changes

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,13 +1,12 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/auth"
+const API_URL = import.meta.env.VITE_BASE_URL
 
 export const loginUserAPI = async (data: {
   email: string
   password: string
 }) => {
 
-  const response = await axios.post(`${API_URL}/login`, data)
-
+  const response = await axios.post(`${API_URL}/authenticate`, data)
   return response.data
 }
